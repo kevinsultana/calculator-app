88df4c621898e972b71f2f35f232068e406a1d93
kevinsultana <kevinpdg.ks@gmail.com>
2025-05-29 21:54:21 +0700

    feat: added calculate function phase 1

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ toggleBtn.addEventListener("click", () => {
 const display = document.getElementById("display");
 
 function appendValue(value) {
-  display.value += value;
+  if (display.value === "Error") {
+    display.value = value;
+  } else {
+    display.value += value;
+  }
 }
 
 function deleteLast() {
@@ -15,17 +19,73 @@ function deleteLast() {
 }
 
 function clearDisplay() {
-  display.value = "";
+  if (display.value && display.value !== "") {
+    let isConfirmed = confirm("Are you sure you want to clear the display?");
+    if (isConfirmed) {
+      display.value = "";
+    }
+    return;
+  }
 }
 
 function appendOperator(operator) {
-  display.value += operator;
+  const lastChar = display.value.slice(-1);
+  const isOperator = ["+", "-", "*", "/"].includes(lastChar);
+
+  if (display.value === "" && operator !== "-") return;
+
+  if (isOperator) {
+    display.value = display.value.slice(0, -1) + operator;
+  } else {
+    display.value += operator;
+  }
 }
 
 function calculate() {
+  const lastChar = display.value.slice(-1);
+  const isOperator = ["+", "-", "*", "/"].includes(lastChar);
   try {
+    if (isOperator) {
+      alert("Please enter a valid number first.");
+      return;
+    } else if (display.value === "") {
+      alert("Please enter a valid number first.");
+      return;
+    }
     display.value = eval(display.value);
   } catch (error) {
     display.value = "Error";
   }
 }
+
+window.addEventListener("keypress", (event) => {
+  console.log(event.key);
+
+  if (event.key >= 0 && event.key <= 9) {
+    appendValue(event.key);
+  }
+  if (event.key === "Enter") {
+    calculate();
+  }
+  if (event.key === "+") {
+    appendOperator("+");
+  }
+  if (event.key === "-") {
+    appendOperator("-");
+  }
+  if (event.key === "*") {
+    appendOperator("*");
+  }
+  if (event.key === "/") {
+    appendOperator("/");
+  }
+});
+
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Backspace") {
+    deleteLast();
+  }
+  if (event.key === "Escape") {
+    clearDisplay();
+  }
+});
